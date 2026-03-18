eb1a371a6e3acb41f7053a4357f2871ff430f2b3
german7869 <germandase@gmail.com>
2026-03-18 10:53:16 -0500

    backend RENDER postgre url

--- bq/settings.py
+++ bq/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import dj_database_url
 import os
 from dotenv import load_dotenv
 
@@ -92,7 +93,8 @@ DATABASES = {
         default=os.getenv("DATABASE_URL"),
         conn_max_age=600
     )
-)
+
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
